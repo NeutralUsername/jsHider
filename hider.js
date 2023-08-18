2507d1864a34f3bf2c3b26ddc37bee8c02350528
NeutralUsername <stuck.fabian@gmail.com>
2023-08-18 20:16:02 +0200

    first commit

---
+++ hider.js
@@ -0,0 +1,53 @@
+var JavaScriptObfuscator = require('javascript-obfuscator');
+const { readFile } = require('fs/promises');
+var fs = require("fs");
+
+function basename(path) {
+    return path.split('/').reverse()[0];
+ }
+
+(async ()=>{
+    if (process.argv.length < 3) {
+        console.log("no starting point provided");
+        return;
+    }
+    const moveFrom = process.argv[2];
+    const saveToPath = process.cwd();
+    if (!fs.existsSync(moveFrom)) {
+        console.log("no dir ", moveFrom);
+        return;
+    }
+    if (fs.statSync(moveFrom).isFile()) {
+        obfuscateFile(moveFrom, saveToPath)
+        return;
+    }
+    if (fs.statSync(moveFrom).isDirectory()) {
+        obfuscateDirectory(moveFrom, saveToPath);
+        return;
+    }
+})();
+
+
+function obfuscateText(fileText){
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(fileText,{
+            compact: false,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 1,
+            numbersToExpressions: true,
+            simplify: true,
+            stringArrayShuffle: true,
+            splitStrings: true,
+            stringArrayThreshold: 1
+        });
+    return obfuscationResult.getObfuscatedCode();
+}
+
+async function obfuscateFile(filePath, saveToPath){
+    fs.writeFileSync(saveToPath + "/" + basename(filePath).replace(".js", "_obfuscated.js"), obfuscateText(await readFile(filePath, 'utf8')));
+}
+
+async function obfuscateDirectory(dirPath, oldSaveToPath){
+    let newSaveToPath = oldSaveToPath + "/" + basename(dirPath) + "_obfuscated";
+    fs.mkdirSync(newSaveToPath);
+        //WIP
+}
